51435afda5ec171f23a8ffd310e5ef83ac38fcc5
LucasRDS2007 <lucasramosdossantos07@gmail.com>
2026-09-22 21:45:02 -0300

    feat:adiciona-fonts-colors-formatacao

--- lib/shared/app_text_style.dart
+++ lib/shared/app_text_style.dart
@@ -2,13 +2,15 @@ import 'package:flutter/material.dart';
 
 class AppTextStyle {
   static TextStyle tittle = TextStyle(
-    fontSize: 20,
+    fontSize: 24,
+    fontFamily: 'Barlow',
     fontWeight: FontWeight.bold,
   );
   static TextStyle subTittle = TextStyle(
     fontSize: 16,
+    fontFamily: 'Barlow',
     fontWeight: FontWeight.bold,
   );
-  static TextStyle body = TextStyle(fontSize: 12);
-  static TextStyle bodySmall = TextStyle(fontSize: 8);
+  static TextStyle body = TextStyle(fontSize: 12, fontFamily: 'Barlow');
+  static TextStyle bodySmall = TextStyle(fontSize: 8, fontFamily: 'Barlow');
 }
