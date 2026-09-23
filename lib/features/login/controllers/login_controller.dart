d04718a05499a4d0cd2881ad3172dee7a956c479
Emanuel <emanuelderrossif@gmail.com>
2026-09-22 21:53:05 -0300

    feat:adicionar-login-page

--- lib/features/login/controllers/login_controller.dart
+++ lib/features/login/controllers/login_controller.dart
@@ -1,3 +1,53 @@
+import 'package:barberhub/shared/mocks/mock_json.dart';
+// import 'package:barberhub/shared/models/User.dart';
+// import 'package:barberhub/shared/models/client.dart';
 import 'package:flutter/material.dart';
 
-class LoginController extends ChangeNotifier {}
+class LoginController extends ChangeNotifier {
+  final int _passwordMinimumLength = 6;
+  final RegExp _emailRegex = RegExp(r'^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$');
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+
+  final GlobalKey<FormState> key = GlobalKey<FormState>();
+
+  bool get isPasswordValid =>
+      passwordController.text.length >= _passwordMinimumLength;
+
+  bool get isEmailValid => _emailRegex.hasMatch(emailController.text);
+
+  UserType? login() {
+    final email = emailController.text;
+    final password = passwordController.text;
+
+    final client = mockClients.firstWhere(
+      (user) => user.email == email && user.password == password,
+    );
+    if (mockClients.isNotEmpty) {
+      return UserType.client;
+    }
+
+    final barber = mockBarbers.firstWhere(
+      (user) => user.email == email && user.password == password,
+    );
+    if (mockBarbers.isNotEmpty) {
+      return UserType.barber;
+    }
+  }
+
+  String? validateEmail(String? value) {
+    if (isEmailValid) {
+      return null;
+    }
+    return 'Email inválido';
+  }
+
+  String? validateSenha(String? value) {
+    if (isPasswordValid) {
+      return null;
+    }
+    return 'Senha invalida';
+  }
+}
+
+enum UserType { client, barber }
--- lib/features/login/pages/login_page.dart
+++ lib/features/login/pages/login_page.dart
@@ -1,6 +1,9 @@
+import 'package:barberhub/features/login/controllers/login_controller.dart';
 import 'package:barberhub/shared/app_colors.dart';
 import 'package:barberhub/shared/app_text_style.dart';
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class LoginPage extends StatelessWidget {
   const LoginPage({super.key});
@@ -11,12 +14,95 @@ class LoginPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(
-          'Teste',
-          style: AppTextStyle.tittle.copyWith(color: AppColors.grey),
-        ),
+        title: Text('Login', style: AppTextStyle.tittle),
+        backgroundColor: AppColors.black,
       ),
-      body: Container(),
+      body: Consumer<LoginController>(
+        builder: (context, loginController, child) {
+          return Form(
+            key: loginController.key,
+            child: SafeArea(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: TextField(
+                      controller: loginController.emailController,
+
+                      decoration: InputDecoration(
+                        labelText: 'email@dominio.com',
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(25),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: TextField(
+                      controller: loginController.passwordController,
+                      obscureText: true,
+
+                      decoration: InputDecoration(
+                        labelText: '****************',
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(25),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: AppColors.grey,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(25),
+                        ),
+                      ),
+                      onPressed: () {
+                        // vai pra home page
+                      },
+                      child: Text('Login'),
+                    ),
+                  ),
+                  Spacer(flex: 2),
+                  RichText(
+                    textAlign: TextAlign.center,
+                    text: TextSpan(
+                      children: [
+                        TextSpan(
+                          text: 'Termos de Serviço ',
+                          style: TextStyle(color: Colors.black),
+                          recognizer: TapGestureRecognizer()
+                            ..onTap = () {
+                              print('cliquei');
+                            },
+                        ),
+                        TextSpan(
+                          text: ' e ',
+                          style: TextStyle(color: Colors.black),
+                        ),
+
+                        TextSpan(
+                          text: ' Politicas de Privacidade',
+                          style: TextStyle(color: Colors.black),
+                          recognizer: TapGestureRecognizer()
+                            ..onTap = () {
+                              print('cliquei');
+                            },
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          );
+        },
+      ),
     );
   }
 }
