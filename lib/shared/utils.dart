a566bf832aff3c55b6d8d82d5d818d35aa15ffca
LucasRDS2007 <lucasramosdossantos07@gmail.com>
2026-09-24 20:09:29 -0300

    feat:atualiza-utils

--- lib/shared/utils.dart
+++ lib/shared/utils.dart
@@ -9,4 +9,24 @@ class Utils {
   static String dateFormat(DateTime date) {
     return DateFormat('dd/MM/yy').format(date);
   }
+
+  static String hourFormat(DateTime date) {
+    return DateFormat('HH:mm').format(date);
+  }
+
+  static String durationFormat(int minutes) {
+    final hours = minutes ~/ 60;
+    final remainingMinutes = minutes % 60;
+
+    if (hours == 0) {
+      return '$minutes ${minutes == 1 ? 'minuto' : 'minutos'}';
+    }
+
+    if (remainingMinutes == 0) {
+      return '$hours ${hours == 1 ? 'hora' : 'horas'}';
+    }
+
+    return '$hours ${hours == 1 ? 'hora' : 'horas'} e '
+        '$remainingMinutes ${remainingMinutes == 1 ? 'minuto' : 'minutos'}';
+  }
 }
